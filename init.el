ece90955c1ab40de95b3fb90db6a925ada4086e1
mathewmarcus <mathewmarcus456@gmail.com>
2017-06-06 17:50:35 -0500

    moved themes to plugins directory and mofied custom-theme-load-path accordingly

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
   (setq mac-option-modifier nil))
 
 ;; Load custom theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/plugins")
 (load-theme 'hober t)
 
 ;; setup generic autocomplete
@@ -104,4 +105,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
